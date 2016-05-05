52fddf2feb41636c6bd09380c2ade2b0627f18dc
ZZMarquis <zhonglingjian3821@163.com>
2016-05-05 16:24:43 +0800

    Revert "commint my code"
    This reverts commit 7efd9b1735f6e85e77c877267df8c06300cbef28.

--- libevent_srv/buildtags.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-ctags -R -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --c++-kinds=+p --fields=+iaS --extra=+q .
-cscope -Rbq -f libevent_srv.out
